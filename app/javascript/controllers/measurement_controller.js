a24856bfd1e2955f70a0f4896c6d67dc132de6af
ShadowCrafter011 <lkoe@bluewin.ch>
2023-04-19 13:36:19 +0200

    Fixed consumer script not found

--- app/javascript/controllers/measurement_controller.js
+++ app/javascript/controllers/measurement_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import consumer from "../channels/consumer"
+import consumer from "channels/consumer"
 import $ from "jquery"
 
 // Connects to data-controller="measurement"
