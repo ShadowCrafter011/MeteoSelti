3e1b8cbc2e36eb6e3ce48da7c0ac8a4056b5549c
ShadowCrafter011 <lkoe@bluewin.ch>
2023-05-30 10:41:45 +0200

    Fixed locale cookie expiring with session

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -6,7 +6,7 @@ class LanguageController < ApplicationController
     return_to = url_from(params[:p]) || root_path
     redirect_to return_to unless I18n.available_locales.include? params[:locale]
 
-    cookies[:locale] = params[:locale].to_sym
+    cookies[:locale] = { value: params[:locale].to_sym, expires: 20.years.from_now }
   end
 
   def locale
